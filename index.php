64ad16ce925139e380e4411db030ae519ffff1b4
王旭杰 <280597526@qq.com>
2017-10-30 10:35:22 +0800

    恢复http不用https

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
         <meta name="keywords" content="">
     </head>
     <script>
-        var wsServer = 'wss://23.105.213.196:9502';
+        var wsServer = 'ws://23.105.213.196:9502';
         var websocket = new WebSocket(wsServer);
         websocket.onopen = function (evt) {
             console.log("Connected to WebSocket server.");
