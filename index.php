877a7117718f301455fe62d4c3e9bfb48bfcb7c2
王旭杰 <280597526@qq.com>
2017-10-30 10:17:40 +0800

    websocket

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php ?>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+        <meta name="description" content="">
+        <meta name="keywords" content="">
+    </head>
+    <script>
+        var wsServer = 'ws://23.105.213.196:9502';
+        var websocket = new WebSocket(wsServer);
+        websocket.onopen = function (evt) {
+            console.log("Connected to WebSocket server.");
+        };
+
+        websocket.onclose = function (evt) {
+            console.log("Disconnected");
+        };
+
+        websocket.onmessage = function (evt) {
+            console.log('Retrieved data from server: ' + evt.data);
+        };
+
+        websocket.onerror = function (evt, e) {
+            console.log('Error occured: ' + evt.data);
+        };
+    </script>
+    <body>
+
+    </body>
+</html>
