05f919dee6242d794a163d49cae3415b2d203145
王旭杰 <280597526@qq.com>
2017-10-30 15:33:13 +0800

    聊天室初现样子

--- index.php
+++ index.php
@@ -22,14 +22,14 @@
             };
 
             websocket.onmessage = function (evt) {
-                if (!evt.data.match("^\{(.+:.+,*){1,}\}$")){
+                if (!evt.data.match("^\{(.+:.+,*){1,}\}$")) {
                     //普通字符串处理
-                    var html_append = '<p class="c-p c-center">'+evt.data+'</p>';
+                    var html_append = '<p class="c-p c-center"><span class="cp-span">' + evt.data + '</span></p>';
                     $('.content').append(html_append);
-                }else{
+                } else {
                     //通过这种方法可将字符串转换为对象
                     var retrieve_data = jQuery.parseJSON(evt.data);
-                    var html_append =  '<p class="c-p c-left"><img src="./static/images/2.jpg" class="avator"><span class="cp-span">'+retrieve_data.data+'</span></p>'
+                    var html_append = '<p class="c-p c-left"><img src="./static/images/2.jpg" class="avator"><span class="cp-span">' + retrieve_data.data + '</span></p>'
                     $('.content').append(html_append);
                 }
             };
@@ -38,16 +38,24 @@
                 console.log('报错: ' + evt.data);
             };
             $("#send").click(function () {
+                send_to_server();
+            });
+            $("#con").keydown(function (event) {
+                if (event.keyCode == 13) {
+                    send_to_server();
+                }
+            });
+            function send_to_server(){
                 var con = $("#con").val();
                 if (con) {
                     websocket.send(con);
                     $("#con").val("");
-                    var html_append = '<p class="c-p c-right"><span class="cp-span">'+con+'</span><img src="./static/images/2.png" class="avator"></p>'
+                    var html_append = '<p class="c-p c-right"><span class="cp-span">' + con + '</span><img src="./static/images/2.png" class="avator"></p>'
                     $(".content").append(html_append);
                 } else {
                     alert("输入格式有误");
                 }
-            });
+            }
         });
 
     </script>
@@ -57,7 +65,7 @@
                 <div class="content">
                 </div>
                 <div class="chatting navbar-fixed-bottom">
-                    <form class="form-inline">
+                    <form class="form-inline" onsubmit="return false;">
                         <div class="form-group">
                             <label for="con">输入框：</label>
                             <input type="text" class="form-control chat-con" id="con" placeholder="请在此输入聊天内容">
