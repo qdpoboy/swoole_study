44fc95dc4f697ad096c8a82dd887e94fba3a0a49
王旭杰 <280597526@qq.com>
2017-10-31 15:17:30 +0800

    文本转超链接

--- server/websocket.php
+++ server/websocket.php
@@ -17,7 +17,7 @@ $ws->on('open', function ($ws, $request) {
 $ws->on('message', function ($ws, $frame) {
     //echo "Message: {$frame->data}\n";
     $push_data = [
-        'data' => $frame->data,
+        'data' => urlToLink($frame->data),
         'append' => 'add',
     ];
     // $ws->connections 遍历所有websocket连接用户的fd，给所有用户推送
@@ -35,3 +35,53 @@ $ws->on('close', function ($ws, $fd) {
 
 $ws->start();
 
+function urlToLink($str) {
+    $arr = array("www." => "http://www.");
+    $str = strtr($str, $arr);
+    $arr = array("http://http://" => "http://");
+    $str = strtr($str, $arr);
+    $str2 = explode("http://", $str);
+    $url  = '';
+    for ($n = 1; isset($str2[$n]); $n ++) {
+        $str3 = explode(".", $str2[$n]);
+        if (isset($str3[1])) {
+            $str4 = explode("www.", $str2[$n]);
+            if ((isset($str4[1]) && isset($str3[2])) || !isset($str4[1])) {
+                $length = strlen($str2[$n]);
+                for ($i = 0; $i <= $length; $i ++) {
+                    //从空格断开 
+                    if (($i - 1) == strlen(trim(mb_substr($str2[$n], 0, $i, 'gb2312')))) {
+                        $ii = $i - 1;
+                        $url1 = mb_substr($str2[$n], 0, $ii, 'gb2312');
+                        $url2 = mb_substr($str2[$n], $ii, $length, 'gb2312');
+                        $url3 = "<a href=\"http://" . $url1 . "\" target=\"_blank\">http://" . $url1 . "</a>" . $url2;
+                        break;
+                    }
+
+                    //从出现汉字处断开 
+                    if ($i != strlen(mb_substr($str2[$n], 0, $i, 'gb2312'))) {
+                        $ii = $i - 1;
+                        $url1 = mb_substr($str2[$n], 0, $ii, 'gb2312');
+                        $url2 = mb_substr($str2[$n], $ii, $length, 'gb2312');
+                        $url3 = "<a href=\"http://" . $url1 . "\" target=\"_blank\">http://" . $url1 . "</a>" . $url2;
+                        break;
+                    }
+                    if ($i == $length) {
+                        $url3 = "<a href=\"http://" . $str2[$n] . "\" target=\"_blank\">http://" . $str2[$n] . "</a>";
+                    }
+                }
+            } else {
+                $url3 = "http://" . $str2[$n];
+            }
+        } else {
+            $url3 = "http://" . $str2[$n];
+        }
+        $url .= $url3;
+    }
+    if (substr($str, 0, 7) == "http://") {
+        $url = "<a href=\"http://$str2[0]\" target=\"_blank\">" . $str2[0] . "</a>" . $url;
+    } else {
+        $url = $str2[0] . $url;
+    }
+    return $url;
+}
