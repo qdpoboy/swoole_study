c7b294f788a2886de49f535bdb385c2523a11fd6
王旭杰 <280597526@qq.com>
2018-04-30 11:51:09 +0800

    测试

--- wgame/fight.php
+++ wgame/fight.php
@@ -13,20 +13,20 @@ class fight {
         if ($m1) {
             $m2 = $this->map2($ws, $frame);
             if ($m2) {
-                return 1;
+                $this->run($ws, $frame);
             }
         }
     }
 
     function map1($ws, $frame) {
         $ws->push($frame->fd, '张三进入到地图1');
-        usleep(100000);
+        usleep(500000);
         $ws->push($frame->fd, '张三遇到了 小狐狸1');
-        usleep(200000);
+        usleep(500000);
         $ws->push($frame->fd, '张三遇到了 小狐狸2');
-        usleep(300000);
+        usleep(500000);
         $ws->push($frame->fd, '张三遇到了 小狐狸3');
-        usleep(400000);
+        usleep(500000);
         $ws->push($frame->fd, '张三遇到了 小狐狸4');
         usleep(500000);
         $ws->push($frame->fd, '张三遇到了 小狐狸5');
@@ -35,9 +35,13 @@ class fight {
 
     function map2($ws, $frame) {
         $ws->push($frame->fd, '张三进入到地图2');
+        usleep(500000);
         $ws->push($frame->fd, '张三遇到了 大狐狸2');
+        usleep(500000);
         $ws->push($frame->fd, '张三遇到了 大狐狸3');
+        usleep(500000);
         $ws->push($frame->fd, '张三遇到了 大狐狸4');
+        usleep(500000);
         $ws->push($frame->fd, '张三遇到了 大狐狸5');
         return 1;
     }
